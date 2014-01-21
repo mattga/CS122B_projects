495ed1c6b2eb93a20f403cc63608302b43ef6003
Matt Gardner <mgardner091@gmail.com>
2014-01-20 22:12:34 -0800

    Types directory in /classes. Updated makefile.

--- Project2/WEB-INF/sources/Movies.java
+++ Project2/WEB-INF/sources/Movies.java
@@ -1,37 +1,61 @@
-import Types.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.sql.*;
 
-/**
- * Use this class to encapsulate behavior of queries 
- * related to Movies. This is a stand alone class that is called
- * from jsp files. 
- * 
- * Use as a Java Bean inside JSP files:
- * {@code <jsp:useBean name="REF_NAME"  class="Movies"/>}
- */
-public class Movies {	
-	private MySQL _sql;
-	/**
-	 * Set up the Database Connection.
-	 */
-	public Movies(){
-		_sql = MySQL.getInstance();
+public class Movies extends HttpServlet {
+	private Connection conn;
+	private Statement statement;
+	private PreparedStatement pstmt;
+
+	public Movies() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb?user=moviedb&password=tiger");
+			statement = conn.createStatement();
+
+			pstmt = conn.prepareStatement("SELECT * FROM movies AS m, genres_in_movies AS gim, genres AS g WHERE m.id = gim.movie_id AND gim.genre_id = g.id AND g.name = ?");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	/**
-	 * Return a String-Array of the Genres.
-	 * 
-	 * @return String[]
-	 */
-	public String[] getGenres() {
-		return null;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		String genre = request.getParameter("genre");
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = genre + " Movies";
+		String docType =
+		"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+		"Transitional//EN\">\n";
+		out.println(docType +
+			"<HTML>\n" +
+			"<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" +
+			"<BODY BGCOLOR=\"#FDF5E6\">\n" +
+			"<H1>" + title + "</H1>");
+
+		try {
+			pstmt.setString(1,genre);
+			ResultSet rs = pstmt.executeQuery();
+
+			if (!rs.first())
+				out.println("<I>No Movies</I>");
+			else {
+				out.println("<UL>");
+				for(; !rs.isAfterLast(); rs.next())
+					out.println("<LI>" + rs.getString("title"));
+				out.println("</UL>");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		out.println("</BODY></HTML>");
 	}
-	
-	/**
-	 * Returns a Movie-Array of the movies.
-	 * @return Movie[]
-	 */
-	public Movie[] getAllMovies() {
-		return null;
-	}
-	
 }
