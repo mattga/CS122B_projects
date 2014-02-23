4dc2b1f253634fda38beed2a31007a88c7cfab05
Matt Gardner <mgardner091@gmail.com>
2014-02-23 11:14:19 -0800

    Validation added + more implementation

--- Project4/src/Main.java
+++ Project4/src/Main.java
@@ -1,6 +1,17 @@
+import java.sql.SQLException;
+
+import Helpers.MySQL;
 
 public class Main {
 	public static void main (String [] args) {
-		
+		try {
+			System.out.println(MySQL.insertBooktitle("book title1"));
+			System.out.println(MySQL.insertGenre("genre1"));
+			
+			DocumentsXMLParser parser = new DocumentsXMLParser();
+			parser.parseFile("res/dblp-data-small/final-data.xml");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
