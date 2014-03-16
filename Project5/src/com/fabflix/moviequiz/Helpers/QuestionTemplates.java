5b91c53acd67f70dc3ca80b8c1ba15619d1618d6
Matt Gardner <mgardner091@gmail.com>
2014-03-16 12:26:32 -0700

    Question 5 and testing of all questions

--- Project5/src/com/fabflix/moviequiz/Helpers/QuestionTemplates.java
+++ Project5/src/com/fabflix/moviequiz/Helpers/QuestionTemplates.java
@@ -9,7 +9,7 @@ public class QuestionTemplates {
 															"Which star was in the movie %s?",
 															"Which star was not in the movie %s?",
 															"In which movie do the stars %s and %s appear together?",
-															"Who directed the movie %s?",
+															"Who directed the star %s?",
 															"Which star appears in both movies %s and %s?",
 															"Which star did not appear in the same movie with the star %s?",
 															"Who directed the star %s in the year %s?"};
